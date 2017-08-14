c6126130fb99a2380b55d507364820278395b226
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-08-14 11:06:55 +0100

    Revert "addgit,  sublime & debian aliases"
    This reverts commit b8de9d9ec75cf1d53e1dc9e85559872c3af6d110.

--- fish/conf.d/deb.fish
+++
@@ -1,7 +0,0 @@
-alias ai='sudo apt-get install'
-
-alias agu='sudo apt-get update'
-
-alias agr='sudo apt-get remove'
-
-alias windows='sudo mount /dev/sda2 /mnt/win10; and cd /mnt/win10'
--- fish/conf.d/sublime.fish
+++
@@ -1,3 +0,0 @@
-function ed
-  subl $argv;
-end
