b8de9d9ec75cf1d53e1dc9e85559872c3af6d110
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-08-14 11:06:27 +0100

    addgit,  sublime & debian aliases

---
+++ fish/conf.d/git.fish
@@ -0,0 +1,22 @@
+
+alias gi='git init'
+
+alias gdinit='sudo rm -rf .git'
+
+alias ga='git add'
+
+alias gcmsg='git commit -m'
+
+alias glg='git log'
+
+alias commits='git log --oneline'
+
+alias chkcommits='commits | grep $var'
+
+alias ids='cut -c1-7'
+    
+alias gsts='git status'
+
+alias gp='git push origin'
+
+alias gpm='git push origin master'
