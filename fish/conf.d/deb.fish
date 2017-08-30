cedfd4510852b91f979d071658368e4e2d91be70
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-08-30 09:46:56 +0100

    add sudo apt-get as an alias

--- fish/conf.d/deb.fish
+++ fish/conf.d/deb.fish
@@ -1,11 +1,11 @@
+alias ag='sudo apt-get'
+
 alias ai='sudo apt-get install'
+
+alias aga='sudo apt-get autoremove'
 
 alias agu='sudo apt-get update'
 
 alias agr='sudo apt-get remove'
 
 alias windows='sudo mount /dev/sda2 /mnt/win10; and cd /mnt/win10'
-
-set fish_greeting "
-  hey ther
-"
